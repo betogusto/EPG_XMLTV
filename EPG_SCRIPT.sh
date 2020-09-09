825d2c3d3ba856b649270fe0e05b1cb1f8702658
betogusto <betogusto2504@gmail.com>
2020-09-09 02:00:05 -0300

    EPG

---
+++ EPG_SCRIPT.sh
@@ -0,0 +1,115 @@
+#!bin/bash
+
+sed -i 's/Animal Planet/ANIMALPLANET/g' epg.xml
+sed -i 's/Aparecida/APARECIDA/g' epg.xml
+sed -i 's/Band/BANDEIRANTES/g' epg.xml
+sed -i 's/Band News/BANDNEWS/g' epg.xml
+sed -i 's/Band Sports/BANDSPORTS/g' epg.xml
+sed -i 's/Boa Vontade/BOAVONTADETV/g' epg.xml
+sed -i 's/Boomerang/BOOMERANG/g' epg.xml
+sed -i 's/Canal Brasil/CANALBRASIL/g' epg.xml
+sed -i 's/Canal do Boi/CANALDOBOI/g' epg.xml
+sed -i 's/Canal Rural/CANALRURAL/g' epg.xml
+sed -i 's/Canal Sony/SONY/g' epg.xml
+sed -i 's/Cancao Nova/CANONOVA/g' epg.xml
+sed -i 's/Cartoon Network/CARTOONNETWORK/g' epg.xml
+sed -i 's/Cinemax/CINEMAX/g' epg.xml
+sed -i 's/Combate/COMBATE/g' epg.xml
+sed -i 's/Comedy Central/COMEDYCENTRAL/g' epg.xml
+sed -i 's/Curta/CURTA/g' epg.xml
+sed -i 's/Discovery Channel/DISCOVERYCHANNEL/g' epg.xml
+sed -i 's/Discovery Home Health/DISCOVERYHH/g' epg.xml
+sed -i 's/Discovery Kids/DISCOVERYKIDS/g' epg.xml
+sed -i 's/Discovery Civilization HD/DISCOVERYCHANNEL/g' epg.xml
+sed -i 's/Discovery Science/DISCOVERYSCIENCE/g' epg.xml
+sed -i 's/Discovery Theater HD/DISCOVERYTHEATERHD/g' epg.xml
+sed -i 's/Discovery Turbo/DISCOVERYTURBO/g' epg.xml
+sed -i 's/Discovery World HD/DISCOVERYWORLD/g' epg.xml
+sed -i 's/Disney Channel/DISNEY/g' epg.xml
+sed -i 's/Disney Junior HD/DISNEYJUNIOR/g' epg.xml
+sed -i 's/Disney XD/DISNEYXD/g' epg.xml
+sed -i 's/E! Entertainment Television/E/g' epg.xml
+sed -i 's/Animal Planet/ANIMALPLANET/g' epg.xml
+sed -i 's/ESPN +/ESPN+/g' epg.xml
+sed -i 's/ESPN Brasil/ESPNBRASIL/g' epg.xml
+sed -i 's/ESPN Extra/ESPNEXTRAHD/g' epg.xml
+sed -i 's/Fish TV/FISHTV/g' epg.xml
+sed -i 's/Food Network/FOODNETWORKHD/g' epg.xml
+sed -i 's/Fox Life/FOXLIFE/g' epg.xml
+sed -i 's/Fox Sports/FOXSPORTS/g' epg.xml
+sed -i 's/Fox Premium 1 HD/FOXPREMIUM1HD/g' epg.xml
+sed -i 's/Fox Premium 2 HD/FOXPREMIUM2HD/g' epg.xml
+sed -i 's/Futura/FUTURA/g' epg.xml
+sed -i 's/Globo News/GLOBONEWS/g' epg.xml
+sed -i 's/Globo SP/GLOBOSP/g' epg.xml
+sed -i 's/Gloob/GLOOB/g' epg.xml
+sed -i 's/HBO 2/HBO2/g' epg.xml
+sed -i 's/HBO Family/HBOFAMILY/g' epg.xml
+sed -i 's/HBO Plus/HBOPLUS/g' epg.xml
+sed -i 's/HBO Signature/HBOSIGNATURE/g' epg.xml
+sed -i 's/History Channel HD/THEHISTORYCHANNEL/g' epg.xml
+sed -i 's/ID - Investigacao Discovery/IDTVHD/g' epg.xml
+sed -i 's/Lifetime/LIFETIME/g' epg.xml
+sed -i 's/Mais Globosat/GLOBOSATHD/g' epg.xml
+sed -i 's/MAX UP/MAXUP/g' epg.xml
+sed -i 's/MAX Prime/MAXPRIMEHD/g' epg.xml
+sed -i 's/Megapix/MEGAPIX/g' epg.xml
+sed -i 's/MTV HD/MTVLIVEHD/g' epg.xml
+sed -i 's/Multishow/MULTISHOW/g' epg.xml
+sed -i 's/Music Box Brazil/MUSICBOXBRAZIL/g' epg.xml
+sed -i 's/National Geographic/NATIONALGEOGRAPHIC/g' epg.xml
+sed -i 's/Natgeo FHD H.265/NATGEOWILDHD/g' epg.xml
+sed -i 's/Nat Geo 4K/NATGEOWILDHD/g' epg.xml
+sed -i 's/NatGeo Wild/NATGEOWILDHD/g' epg.xml
+sed -i 's/NatGeo Kids/NATGEOKIDS/g' epg.xml
+sed -i 's/Nick Jr/NICKJR/g' epg.xml
+sed -i 's/Nickelodeon/NICKELODEON/g' epg.xml
+sed -i 's/NovoTempo/TVNOVOTEMPO/g' epg.xml
+sed -i 's/OFF/OFFHD/g' epg.xml
+sed -i 's/Paramount/PARAMOUNT/g' epg.xml
+sed -i 's/Playboy TV/PLAYBOYTV/g' epg.xml
+sed -i 's/Premiere Clubes/PREMIERECLUBES/g' epg.xml
+sed -i 's/Premiere 2/PREMIERE2/g' epg.xml
+sed -i 's/Premiere 3/PREMIERE3/g' epg.xml
+sed -i 's/Premiere 4/PREMIERE4/g' epg.xml
+sed -i 's/Premiere 5/PREMIERE5/g' epg.xml
+sed -i 's/Premiere 6/PREMIERE6/g' epg.xml
+sed -i 's/Premiere 7/PREMIERE7/g' epg.xml
+sed -i 's/Prime Box Brazil/PRIMEBOXBRAZIL/g' epg.xml
+sed -i 's/Record HD/REDERECORD/g' epg.xml
+sed -i 's/Record News/RECORDNEWS/g' epg.xml
+sed -i 's/Rede Brasil/TVBRASIL/g' epg.xml
+sed -i 's/Rede TV/REDETV/g' epg.xml
+sed -i 's/Rede Vida/REDEVIDA/g' epg.xml
+sed -i 's/Sextreme/SEXTREME/g' epg.xml
+sed -i 's/Sexy Hot/SEXHOT/g' epg.xml
+sed -i 's/Space/SPACE/g' epg.xml
+sed -i 's/SporTV/SPORTV/g' epg.xml
+sed -i 's/SporTV 3/SPORTV3/g' epg.xml
+sed -i 's/SporTV 2/SPORTV2/g' epg.xml
+sed -i 's/Studio Universal/UNIVERSAL/g' epg.xml
+sed -i 's/Syfy/SYFY/g' epg.xml
+sed -i 's/Telecine Action/TELECINEACTION/g' epg.xml
+sed -i 's/Telecine Cult/TELECINECULT/g' epg.xml
+sed -i 's/Telecine Fun/TELECINEFUN/g' epg.xml
+sed -i 's/Telecine Pipoca/TELECINEPIPOCA/g' epg.xml
+sed -i 's/Telecine Premium/TELECINEPREMIUM/g' epg.xml
+sed -i 's/Telecine Touch/TELECINETOUCH/g' epg.xml
+sed -i 's/Terra Viva/TERRAVIVA/g' epg.xml
+sed -i 's/The History Channel/THEHISTORYCHANNEL/g' epg.xml
+sed -i 's/TLC/TRAVELANDLIVINGCHANNEL/g' epg.xml
+sed -i 's/TNT Series/TNTSERIES/g' epg.xml
+sed -i 's/TV Cultura/TVCULTURA/g' epg.xml
+sed -i 's/TV Justiça/TVJUSTIA/g' epg.xml
+sed -i 's/TV Ra tim bum/TVRTIMBUM/g' epg.xml
+sed -i 's/TV Senado/TVSENADO/g' epg.xml
+sed -i 's/TVE/TVESCOLT/g' epg.xml
+sed -i 's/Universal Channel/STUDIOUNIVERSAL/g' epg.xml
+sed -i 's/UNIVERSAL/UNIVERSAL/g' epg.xml
+sed -i 's/Venus/VENUS/g' epg.xml
+sed -i 's/VH1 MegaHits/VH1HD/g' epg.xml
+sed -i 's/Viva/VIVA/g' epg.xml
+sed -i 's/Warner Channel/WARNER/g' epg.xml
+sed -i 's/Woohoo/WOOHOO/g' epg.xml
+sed -i 's/Zoomoo/ZOOMOO/g' epg.xml
+sed -i 's,Animal Planet,ANIMALPLANET,g' epg.xml
